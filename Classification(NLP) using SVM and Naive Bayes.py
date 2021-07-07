81c2ab5936cbad74b6b804221005b6cff44ca246
MartinAguilarR <martin.aguilarr@utem.cl>
2021-07-07 12:10:05 +0200

    Update Classification(NLP) using SVM and Naive Bayes.py

--- Classification(NLP) using SVM and Naive Bayes.py
+++ Classification(NLP) using SVM and Naive Bayes.py
@@ -20,11 +20,18 @@ from sklearn.metrics import accuracy_score
 np.random.seed(500)
 
 # Add the Corpus
-Data = pd.read_csv(r"C:\Users\marti\OneDrive\Desktop\investigacion\TESIS DE GRADO\Finales\RS_Data_Final_sentiments.csv", encoding='UTF-8')
-Corpus = Data.iloc[:,0]
+Corpus = pd.read_csv(r"C:\\Users\\marti\\OneDrive\\Documentos\\GitHub\\Tesis\\Finales\\Df_Final.csv", encoding='latin1')
 print(Corpus)
 
 # Tokenization and Lemmatization
+# Step - a : Remove blank rows if any.
+Corpus["Comentario"].dropna(inplace=True)
+
+# Step - b : Change all the text to lower case. This is required as python interprets 'dog' and 'DOG' differently
+Corpus["Comentario"] = [entry.lower() for entry in Corpus["Comentario"]]
+
+# Step - c : Tokenization : In this each entry in the corpus will be broken into set of words
+Corpus["Comentario"]= [word_tokenize(entry) for entry in Corpus["Comentario"]]
 
 # Step - d : Remove Stop words, Non-Numeric and perfom Word Stemming/Lemmenting.
 # WordNetLemmatizer requires Pos tags to understand if the word is noun or verb or adjective etc. By default it is set to Noun
@@ -32,6 +39,7 @@ tag_map = defaultdict(lambda : wn.NOUN)
 tag_map['J'] = wn.ADJ
 tag_map['V'] = wn.VERB
 tag_map['R'] = wn.ADV
+
 for index,entry in enumerate(Corpus):
     # Declaring Empty List to store the words that follow the rules for this step
     Final_words = []
@@ -45,3 +53,21 @@ for index,entry in enumerate(Corpus):
             Final_words.append(word_Final)
     # The final processed set of words for each iteration will be stored in 'text_final'
     Corpus.loc[index,'text_final'] = str(Final_words)
+
+# TRAINING
+Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(Corpus['text_final'],Corpus['label'],test_size=0.3)
+
+# Encoding de Tokenizado
+Encoder = LabelEncoder()
+Train_Y = Encoder.fit_transform(Train_Y)
+Test_Y = Encoder.fit_transform(Test_Y)
+
+# Vectorización de texto: TF- IDF
+Tfidf_vect = TfidfVectorizer(max_features=5000)
+Tfidf_vect.fit(Corpus['text_final'])
+
+Train_X_Tfidf = Tfidf_vect.transform(Train_X)
+Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+
+print(Tfidf_vect.vocabulary_)
+print(Train_X_Tfidf)
